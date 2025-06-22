f6316957a84b80ffa0daf7e86d76191250c111fa
SiddheshwarKale <siddkale2005@gmail.com>
2025-06-22 12:17:51 +0530

    reset winning score

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ btnhold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add('hidden');
       document
